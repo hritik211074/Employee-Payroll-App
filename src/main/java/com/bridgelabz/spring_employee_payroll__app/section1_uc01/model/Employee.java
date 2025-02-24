815760f4380e0e058409bb14a36873ca36918385
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-24 19:34:02 +0530

    [Hritik] adding the section1_uc01 folder into section1_uc01 branch

---
+++ src/main/java/com/bridgelabz/spring_employee_payroll__app/section1_uc01/model/Employee.java
@@ -0,0 +1,57 @@
+package com.bridgelabz.spring_employee_payroll__app.section1_uc01.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String department;
+    private double salary;
+
+    // Constructors, getters and setters
+    public Employee() {}
+
+    public Employee(String name, String department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
