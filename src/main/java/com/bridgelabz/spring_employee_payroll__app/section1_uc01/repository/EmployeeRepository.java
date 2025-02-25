6e12c4366c5b780fdf148095c732fd1022fc9e11
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-25 10:55:11 +0530

    [Hritik] adding section1_uc01 folder into section1_uc01

--- src/main/java/com/bridgelabz/spring_employee_payroll__app/section1_uc01/repository/EmployeeRepository.java
+++ src/main/java/com/bridgelabz/spring_employee_payroll__app/section1_uc01/repository/EmployeeRepository.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.spring_employee_payroll__app.section1_uc01.repository;
 
 
-import com.payroll.model.Employee;
+import com.bridgelabz.spring_employee_payroll__app.section1_uc01.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     // You can define custom queries here if needed.
